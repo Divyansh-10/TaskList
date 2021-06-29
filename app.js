f6fdae22a0bfce43da9d9a094b7b84e1de2bba71
Divyansh Kamboj <60178033+Divyansh-10@users.noreply.github.com>
2021-06-30 02:31:10 +0530

    Added Express routing and Background customization

--- app.js
+++ app.js
@@ -6,28 +6,34 @@ const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
-app.set("view engine" , "ejs");
+app.set("view engine", "ejs");
 
 //Connecting to mongoose Database server..DB name = toDoListDB
-mongoose.connect("mongodb://localhost:27017/toDoListDB" , {useNewUrlParser : true, useUnifiedTopology: true},
-(err) => {
-if(err) console.log(err);
-else console.log("Successfully connected to database");
+mongoose.connect("mongodb://localhost:27017/toDoListDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  (err) => {
+    if (err) console.log(err);
+    else console.log("Successfully connected to database");
 
-});
+  });
 
 //Creating Schema
 const listSchema = {
-  name : {
-     type : String,
-     required : [true , "Kaa pehelwaan..Naam dalna bhool gye ka?"]
+  name: {
+    type: String,
+    required: [true, "Kaa pehelwaan..Naam dalna bhool gye ka?"]
   }
 }
 
 //Creating Model
-const Item = mongoose.model("Item" , listSchema);
+const Item = mongoose.model("Item", listSchema);
+const Work = mongoose.model("Work", listSchema);
 
 //Adding default items for testing
 // const item1 = new Item({
@@ -45,46 +51,149 @@ const Item = mongoose.model("Item" , listSchema);
 
 
 
-app.get("/", function(req, res){
+//Routes for Item List
+//Root route
+app.get("/:list", function(req, res) {
 
-  var day = date.getDate();
+  let list = req.params.list;
+
+  if(list === "home")
+  {
 
   Item.find({}, (err, result) => {
-       res.render("list" , {day : day , Item : result});
-
+    res.render("list", {Item: result });
   });
+}
+
+  if(list === "work")
+  {
+    Work.find({}, (err, result) => {
+        res.render("workList", { Work: result});
+    });
+  }
+
 });
 
 
+//Compose route
+app.post("/compose/:compose", (req, res) => {
 
-app.post("/compose" , (req,res) =>
-{
+   let compose = req.params.compose;
+
+   if(compose === "home")
+ {
+
   let item = new Item({
-  name : req.body.item
+    name: req.body.item
   });
 
+  Item.create([item], (err) => {
 
-app.post("/delete" , (req,res) => {
-   console.log(req.body.checkbox);
+    if (err) console.log(err);
+    else console.log("Added document to Item database!!");
+
+  });
+
+  res.redirect("/home");
+}
+
+   if(compose === "work")
+   {
+     let work = new Work({
+       name: req.body.item
+     });
+
+     Work.create([work] , (err) => {
+       if(err) console.log(err);
+       else console.log("Insertion to work list successfull!");
+     });
+
+     res.redirect("/work");
+   }
+
+
 });
 
 
 
-  Item.create([item] , (err) => {
+//Delete route
+app.post("/delete/:del", (req, res) => {
+
+  const del = req.params.del;
+
+  if(del === "home")
+  {
+  let del = req.body.checkbox;
+
+  Item.deleteOne({  _id: del }, (err) => {
+
+    if (err) console.log(err);
+    else console.log(del + " Item deleted successfully");
+  });
+
+  res.redirect("/home");
+}
+
+
+
+if(del === "work")
+{
+  let id = req.body.checkbox;
+
+  Work.findByIdAndRemove( id , (err) => {
 
     if(err) console.log(err);
-    else console.log("Added document to database!! --> " , req.body.item);
-
+    else console.log("Successfully Deleted from work List");
   });
 
-  res.redirect("/");
+   res.redirect("/work");
+}
+
+
 });
 
 
 
+//Routes for work list
+// app.get("/work", (req, res) => {
+//
+//       Work.find({}, (err, result) => {
+//           res.render("workList", { Work: result});
+//       });
+//     });
+
+
+// app.post("/composeWork", (req, res) => {
+//
+//       let work = new Work({
+//         name: req.body.item
+//       });
+//
+//       Work.create([work] , (err) => {
+//         if(err) console.log(err);
+//         else console.log("Insertion to work list successfull!");
+//       });
+//
+//       res.redirect("/work");
+//     });
+
+
+// app.post("/deleteWork" , (req,res) => {
+//
+//    let id = req.body.checkbox;
+//
+//    Work.findByIdAndRemove( id , (err) => {
+//
+//      if(err) console.log(err);
+//      else console.log("Successfully Deleted from work List");
+//    });
+//
+//     res.redirect("/work");
+//
+// });
 
 
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.....");
-});
+app.listen(3000, () => {
+      console.log("Server started on port 3000.....");
+    });
